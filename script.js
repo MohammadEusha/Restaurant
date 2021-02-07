4bfe4c456ae6436756d943a6b2de68e9deb1d3e1
Mohammad Eusha <eusha37@gmail.com>
2021-02-07 22:38:52 +0600

    Ending

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
 const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-details-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
+const foodList = document.getElementById('food');
+const foodDetailsContent = document.querySelector('.food-details-container');
+const ingredientsCloseBtn = document.getElementById('ingredients-close-btn');
 
 
-searchBtn.addEventListener('click', getMealList);
-mealList.addEventListener('click', getMealRecipe);
-recipeCloseBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showRecipe');
+searchBtn.addEventListener('click', getFoodList);
+foodList.addEventListener('click', getFoodIngredients);
+ingredientsCloseBtn.addEventListener('click', () => {
+    foodDetailsContent.parentElement.classList.remove('showIngredients');
 });
 
 
 
-function getMealList(){
+function getFoodList(){
     let searchInputTxt = document.getElementById('search-input').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
         let html = "";
         if(data.meals){
             data.meals.forEach(meal => {
                 html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
+                    <div class = "food-item" data-id = "${meal.idMeal}">
+                        <div class = "food-img">
                             <img src = "${meal.strMealThumb}" alt = "food">
                         </div>
-                        <div class = "meal-name">
+                        <div class = "food-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            <a href = "#" class = "ingredients-btn">Get ingredients</a>
                         </div>
                     </div>
                 `;
             });
-            mealList.classList.remove('notFound');
+            foodList.classList.remove('notFound');
         } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
+            html = "Sorry, we didn't find any Food!";
+            foodList.classList.add('notFound');
         }
 
-        mealList.innerHTML = html;
+        foodList.innerHTML = html;
     });
 }
 
 
 
-function getMealRecipe(e){
+function getFoodIngredients(e){
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+    if(e.target.classList.contains('ingredients-btn')){
+        let foodItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodItem.dataset.id}`)
         .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+        .then(data => foodIngredientsModal(data.meals));
     }
 }
 
 
-function mealRecipeModal(meal){
-    console.log(meal);
+function foodIngredientsModal(meal){
     meal = meal[0];
     let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Ingredient:</h3>
+        <h2 class = "ingredients-title">${meal.strMeal}</h2>
+        <p class = "ingredients-category">${meal.strCategory}</p>
+        <div class = "ingredients-description">
+            <h3>Ingredients:</h3>
             <p>${meal.strIngredient1}</p>
             <p>${meal.strIngredient2}</p>
             <p>${meal.strIngredient3}</p>
@@ -74,7 +73,10 @@ function mealRecipeModal(meal){
             <p>${meal.strIngredient9}</p>
             <p>${meal.strIngredient10}</p>
         </div>
+        <div class = "ingredients-food-img">
+          <img src = "${meal.strMealThumb}" alt = "">
+        </div>
     `;
-    mealDetailsContent.innerHTML = html;
-    mealDetailsContent.parentElement.classList.add('showRecipe');
+    foodDetailsContent.innerHTML = html;
+    foodDetailsContent.parentElement.classList.add('showIngredients');
 }
